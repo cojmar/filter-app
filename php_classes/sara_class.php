f97c86cf87384b401e2b7c75560bb9bed080f755
alex p <alexandru.p@thoreb.com>
2020-08-06 17:37:28 +0300

    added 5.2.149.9 to no login ip's for testing

--- php_classes/sara_class.php
+++ php_classes/sara_class.php
@@ -1,17 +1,21 @@
 <?php
 class sara_class{
-    function __construct(){ 
+    function __construct(){
+        $this->no_login_ips = array(
+            '127.0.0.1',
+            '5.2.149.9'
+        );
     }
     function check_token($token=false){
-        if($_SERVER['REMOTE_ADDR']==='127.0.0.1') return true;
+        if (in_array($_SERVER['REMOTE_ADDR'],$this->no_login_ips) !== false) return true;
         if (empty($token)) return false;
         $url = 'http://127.0.0.1:8080/SaraSERVER/resources/authorize/user/verify/' . $token;
         $response = file_get_contents($url);
         return (!empty($response))?true:false;
     }
     function check_login(){
-        return true;        
+        return true;
         $token = (!empty($_SESSION['token']))?$_SESSION['token']:false;
-        return $this->check_token($token);        
+        return $this->check_token($token);
     }
 }
