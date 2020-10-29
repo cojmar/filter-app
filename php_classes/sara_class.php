833ec5ef924b3b2da59fbfbd2c299d11e2b97f06
cojmar <cojmar@gmail.com>
2020-10-29 18:01:04 +0200

    small changs

--- php_classes/sara_class.php
+++ php_classes/sara_class.php
@@ -1,23 +1,27 @@
 <?php
-class sara_class{
-    function __construct(){
+class sara_class
+{
+    function __construct()
+    {
         $this->no_login_ips = array(
             //'86.121.165.1',
             '127.0.0.1',
             '5.2.149.9'
         );
     }
-    function check_token($token=false){
+    function check_token($token = false)
+    {
         //return true;
-        if (in_array($_SERVER['REMOTE_ADDR'],$this->no_login_ips) !== false) return true;
+        if (in_array($_SERVER['REMOTE_ADDR'], $this->no_login_ips) !== false) return true;
         if (empty($token)) return false;
         $url = 'http://127.0.0.1:8080/SaraSERVER/resources/authorize/user/verify/' . $token;
         $response = file_get_contents($url);
-        return (!empty($response))?true:false;
+        return (!empty($response)) ? true : false;
     }
-    function check_login(){
+    function check_login()
+    {
         //return true;
-        $token = (!empty($_SESSION['token']))?$_SESSION['token']:false;
+        $token = (!empty($_SESSION['token'])) ? $_SESSION['token'] : false;
         return $this->check_token($token);
     }
 }
