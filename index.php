b6ccc8014116d45f819cca1f67d63a01d9a117be
cojmar <cojmar@gmail.com>
2022-03-11 18:03:02 +0200

    added json files to updater

--- index.php
+++ index.php
@@ -1,245 +1,274 @@
 <?php
-	session_start();
-	class router{
-		//==Routes
-		public function test_sara(){
-			$sara = new sara_class();
-			debug($sara->check_login());
+session_start();
+class router
+{
+	//==Routes
+	public function test_sara()
+	{
+		$sara = new sara_class();
+		debug($sara->check_login());
+	}
+	public function test_json()
+	{
+		$update = new update_class();
+	}
+	protected function cron()
+	{
+		$cron_file = "assets" . DIRECTORY_SEPARATOR . "build" . DIRECTORY_SEPARATOR . "cron.json";
+		$cron_data = (file_exists($cron_file)) ? json_decode(file_get_contents($cron_file), 1) : array(
+			"status" => "done"
+		);
+		debug($cron_data);
+		if ($cron_data['status'] == "working") {
+			return false;
 		}
-		protected function cron(){
-			$cron_file = "assets".DIRECTORY_SEPARATOR."build".DIRECTORY_SEPARATOR."cron.json";
-			$cron_data = (file_exists($cron_file))?json_decode(file_get_contents($cron_file),1):array(
-				"status"=>"done"
-			);
-			debug($cron_data);
-			if ($cron_data['status'] =="working"){
-				return false;
+		$cron_data['status'] = "working";
+		$cron_data['last_date'] = date("Y-m-d H:i:s");
+		file_put_contents($cron_file, json_encode($cron_data));
+		//sleep(10);
+		$this->update();
+		$cron_data['status'] = "done";
+		file_put_contents($cron_file, json_encode($cron_data));
+	}
+	protected function update()
+	{
+		return new update_class();
+	}
+	protected function php_info()
+	{
+		phpinfo();
+		die;
+	}
+	protected function resources()
+	{
+		$res = new resources_class();
+		$out = $res->run($this->url_data);
+		//debug($out);
+		json_output($out);
+	}
+	protected function save_settings()
+	{
+		$json = file_get_contents('php://input');
+		$data = json_decode($json, 1);
+		$out = array();
+		$ok = false;
+		if (!empty($data['settings']) && is_array($data['settings'])) {
+			if (isset($data['settings']['codes'])  && isset($data['settings']['send_interval'])) {
+				$settings = array(
+					'codes' => $data['settings']['codes'],
+					'send_interval' => $data['settings']['send_interval'],
+					'default_natch' => $data['settings']['default_natch']
+				);
+				$ok = true;
+				file_put_contents("assets" . DIRECTORY_SEPARATOR . "build" . DIRECTORY_SEPARATOR . "settings.json", json_encode($settings));
 			}
-			$cron_data['status'] = "working";
-			$cron_data['last_date'] = date("Y-m-d H:i:s");
-			file_put_contents($cron_file,json_encode($cron_data));
-			//sleep(10);
-			$this->update();
-			$cron_data['status'] = "done";
-			file_put_contents($cron_file,json_encode($cron_data));
 		}
-		protected function update(){
-			return new update_class();
-		}
-		protected function php_info(){
-			phpinfo();
-			die;
-		}
-		protected function resources(){
-			$res = new resources_class();
-			$out =$res->run($this->url_data);
-			//debug($out);
-			json_output($out);
-		}
-		protected function save_settings(){
-			$json = file_get_contents('php://input');
-			$data = json_decode($json,1);
-			$out = array();
-			$ok = false;
-			if (!empty($data['settings']) && is_array($data['settings'])){
-				if ( isset($data['settings']['codes'])  && isset($data['settings']['send_interval'])){
-					$settings = array(
-						'codes'=>$data['settings']['codes'],
-						'send_interval'=>$data['settings']['send_interval'],
-						'default_natch'=>$data['settings']['default_natch']
-					);
-					$ok=true;
-					file_put_contents("assets".DIRECTORY_SEPARATOR."build".DIRECTORY_SEPARATOR."settings.json",json_encode($settings));
-				}
-			}
-			$out['status']=($ok)?'saved':'invalid data';
-			json_output($out);
-		}
-		protected function save_email(){
-			$json = file_get_contents('php://input');
-			$data = json_decode($json,1);
-			$out = array();
+		$out['status'] = ($ok) ? 'saved' : 'invalid data';
+		json_output($out);
+	}
+	protected function save_email()
+	{
+		$json = file_get_contents('php://input');
+		$data = json_decode($json, 1);
+		$out = array();
+		$em = new emails_manager_class();
+		$ok = $em->save_email($data);
+		$out['status'] = ($ok) ? 'saved' : 'invalid data';
+		$out['id'] = $ok;
+		json_output($out);
+	}
+	protected function delete_email()
+	{
+		$data = (!empty($this->url_data[0])) ? $this->url_data[0] : false;
+		$out = array('status' => false);
+		if ($data) {
 			$em = new emails_manager_class();
-			$ok = $em->save_email($data);
-			$out['status']=($ok)?'saved':'invalid data';
-			$out['id'] = $ok;
-			json_output($out);
+			$out['status'] = $em->delete_email($data);
 		}
-		protected function delete_email(){
-			$data = (!empty($this->url_data[0]))?$this->url_data[0]:false;
-			$out = array('status'=>false);
-			if ($data){
-				$em = new emails_manager_class();
-				$out['status'] = $em->delete_email($data);
-			}
-			json_output($out);
-		}
-		protected function get_email(){
-			$data = (!empty($this->url_data[0]))?$this->url_data[0]:false;
-			$out = array('error'=>true);
-			if ($data){
-				$em = new emails_manager_class();
-				if ($email_data = $em->get_email_data($data)){
-					$out = $email_data;
-				}
-			}
-			json_output($out);
-		}
-		protected function check_email(){
-			$data = (!empty($this->url_data[0]))?$this->url_data[0]:false;
-			$out = array('status'=>false);
-			if ($data){
-				$em = new emails_manager_class();
-				if ($em->get_email_id($data)){
-					$out['status'] = true;
-				}
-			}
-			json_output($out);
-		}
-		protected function get_emails(){
+		json_output($out);
+	}
+	protected function get_email()
+	{
+		$data = (!empty($this->url_data[0])) ? $this->url_data[0] : false;
+		$out = array('error' => true);
+		if ($data) {
 			$em = new emails_manager_class();
-			$out = $em->get_emails();
-			json_output($out);
-		}
-		protected function import(){
-            $importer = new import_class();
-            $importer->import();
-		}
-		protected function install(){
-			$installer = new install_class();
-			$installer->install();
-			debug('Done.');
-		}
-		protected function build(){
-			$builder = new build_class();
-			$builder->build();
-		}
-		protected function test(){
-			$ret = $this->db->table_columns('codes');
-			if (empty($ret)){
-				debug("DB not working!");
-			}else{
-				debug("DB ok.");
+			if ($email_data = $em->get_email_data($data)) {
+				$out = $email_data;
 			}
 		}
-		public function client(){
-            $out = file_get_contents('client.html');
-            die($out);
-		}
-		public function need_login(){
-			header("HTTP/1.1 401 Unauthorized");
-            die('Unauthorized access, please use portal to login.');
-		}
-		public function check_login(){
-			$sara = new sara_class();
-			$out = array(
-				'login'=>$sara->check_login()
-			);
-			json_output($out);
-		}
-		public function default_route(){
-            header("HTTP/1.0 404 Not Found");
-            die;
-		}
-		//==Custom Init
-		private function init(){
-			$this->db = new db_class();
-			$this->db->connect("localhost","root","Tu#1Fru#1","filter_app");
-			$this->db->connect("localhost","root","asdasd","filter_app");
-			$this->db->connect("localhost","root","thoreb-7","filter_app");
-		}
-		function __construct()
-		{
-			set_time_limit(0);
-			if (isset($_GET['SESSION'])){
-				$_SESSION['token'] = $_GET['SESSION'];
-				header('Location: ./');
-				die;
-			}
-			if (isset($_GET['LOGOFF'])){
-				$_SESSION['token'] = false;
-				header('Location: ./');
-				die;
-			}
-			// PROTECTED functions require login
-			// Get url segments try to find a non private function with same name as 1st segment and run it
-			// If not try to lunch default_route()
-			// $this->route will be the 1st segment,
-			// $this->url_data will be array of rest of segments if any
-			$this->route = "client";//default route on empty segments
-			$this->url_data = array();
-			$ok_run_route = true;
-			if ($route_data = $this->get_route_data()){
-				$this->route = array_shift($route_data);
-				$this->url_data = $route_data;
-			}
-			if (method_exists($this,$this->route) && $this->route !==__FUNCTION__)
-			{
-				$reflection = new ReflectionMethod($this, $this->route);
-				$needs_login = (!$reflection->isPublic());
-				$ok_run_route = ($needs_login)?false:true;
-				if (!$ok_run_route){
-					$sara = new sara_class();
-					$ok_run_route = $sara->check_login();
-				}
-			}
-			else{
-				$this->route = "default_route";
-			}
-			if ($ok_run_route){
-				$route = $this->route;
-				//==Run custom init
-				if (method_exists($this,'init')) $this->init();
-				//==Run route
-				if (method_exists($this,$route)) $this->$route();
-			}else{
-				if (method_exists($this,'need_login')) $this->need_login();
+		json_output($out);
+	}
+	protected function check_email()
+	{
+		$data = (!empty($this->url_data[0])) ? $this->url_data[0] : false;
+		$out = array('status' => false);
+		if ($data) {
+			$em = new emails_manager_class();
+			if ($em->get_email_id($data)) {
+				$out['status'] = true;
 			}
 		}
-		private function get_route_data()
-		{	//==Processes url segments delimited by / returns array of segments or false
-			$path = false;
-			$real_path = substr(urldecode($_SERVER['REQUEST_URI']),strlen(dirname($_SERVER['SCRIPT_NAME'])));
-			$path_info = pathinfo($real_path);
-			$uri_path =(isset($path_info['dirname']) && strlen($path_info['dirname'])>1)?$path_info['dirname']:'';
-			if (!empty($uri_path) && substr($uri_path,0,1)=="/") $uri_path =substr($uri_path,1);
-			if (!empty($uri_path)) $uri_path.="/";
-			$uri_path .=$path_info['basename'];
-			if($path_info['basename'] == $path_info['filename'] && !empty($uri_path)) $path=explode("/",$uri_path);
-			elseif(!empty($path_info['basename'])) $path=explode("/",$uri_path);
-			return $path;
+		json_output($out);
+	}
+	protected function get_emails()
+	{
+		$em = new emails_manager_class();
+		$out = $em->get_emails();
+		json_output($out);
+	}
+	protected function import()
+	{
+		$importer = new import_class();
+		$importer->import();
+	}
+	protected function install()
+	{
+		$installer = new install_class();
+		$installer->install();
+		debug('Done.');
+	}
+	protected function build()
+	{
+		$builder = new build_class();
+		$builder->build();
+	}
+	protected function test()
+	{
+		$ret = $this->db->table_columns('codes');
+		if (empty($ret)) {
+			debug("DB not working!");
+		} else {
+			debug("DB ok.");
 		}
 	}
-	//==Autoload
-	spl_autoload_register(function ($class_name) {
-		include 'php_classes' . DIRECTORY_SEPARATOR . $class_name . '.php';
-	});
-	//==Run
-	ini_set('display_errors', 1);
-	error_reporting(E_ALL);
-	set_time_limit(0);
-	function json_output($data=false){
-		$out = (is_array($data) || is_object($data))?json_encode($data):json_encode(array($data));
-		header('Content-Type: application/json');
+	public function client()
+	{
+		$out = file_get_contents('client.html');
 		die($out);
 	}
-	function debug($data,$mode=NULL)
+	public function need_login()
 	{
-		switch($mode)
-		{
-			default:$ret_data = print_r($data,1);break;
-			case 1:$ret_data = json_encode($data);break;
-			case 2:$ret_data = var_export($data,1);break;
-		}
-		print "<div style='background-color:#efefef;padding:5px;border:1px solid #5d5d5d;border-top:0px;'><xmp>".$ret_data."</xmp></div>";
+		header("HTTP/1.1 401 Unauthorized");
+		die('Unauthorized access, please use portal to login.');
 	}
-	$start_time = microtime(true);
-	ob_start();
-	new router();
-	$output = ob_get_contents();
-	$execution_time = microtime(true) - $start_time;
-	@ob_end_clean();
-	print "
+	public function check_login()
+	{
+		$sara = new sara_class();
+		$out = array(
+			'login' => $sara->check_login()
+		);
+		json_output($out);
+	}
+	public function default_route()
+	{
+		header("HTTP/1.0 404 Not Found");
+		die;
+	}
+	//==Custom Init
+	private function init()
+	{
+		$this->db = new db_class();
+		$this->db->connect("localhost", "root", "Tu#1Fru#1", "filter_app");
+		$this->db->connect("localhost", "root", "asdasd", "filter_app");
+		$this->db->connect("localhost", "root", "thoreb-7", "filter_app");
+	}
+	function __construct()
+	{
+		set_time_limit(0);
+		if (isset($_GET['SESSION'])) {
+			$_SESSION['token'] = $_GET['SESSION'];
+			header('Location: ./');
+			die;
+		}
+		if (isset($_GET['LOGOFF'])) {
+			$_SESSION['token'] = false;
+			header('Location: ./');
+			die;
+		}
+		// PROTECTED functions require login
+		// Get url segments try to find a non private function with same name as 1st segment and run it
+		// If not try to lunch default_route()
+		// $this->route will be the 1st segment,
+		// $this->url_data will be array of rest of segments if any
+		$this->route = "client"; //default route on empty segments
+		$this->url_data = array();
+		$ok_run_route = true;
+		if ($route_data = $this->get_route_data()) {
+			$this->route = array_shift($route_data);
+			$this->url_data = $route_data;
+		}
+		if (method_exists($this, $this->route) && $this->route !== __FUNCTION__) {
+			$reflection = new ReflectionMethod($this, $this->route);
+			$needs_login = (!$reflection->isPublic());
+			$ok_run_route = ($needs_login) ? false : true;
+			if (!$ok_run_route) {
+				$sara = new sara_class();
+				$ok_run_route = $sara->check_login();
+			}
+		} else {
+			$this->route = "default_route";
+		}
+		if ($ok_run_route) {
+			$route = $this->route;
+			//==Run custom init
+			if (method_exists($this, 'init')) $this->init();
+			//==Run route
+			if (method_exists($this, $route)) $this->$route();
+		} else {
+			if (method_exists($this, 'need_login')) $this->need_login();
+		}
+	}
+	private function get_route_data()
+	{	//==Processes url segments delimited by / returns array of segments or false
+		$path = false;
+		$real_path = substr(urldecode($_SERVER['REQUEST_URI']), strlen(dirname($_SERVER['SCRIPT_NAME'])));
+		$path_info = pathinfo($real_path);
+		$uri_path = (isset($path_info['dirname']) && strlen($path_info['dirname']) > 1) ? $path_info['dirname'] : '';
+		if (!empty($uri_path) && substr($uri_path, 0, 1) == "/") $uri_path = substr($uri_path, 1);
+		if (!empty($uri_path)) $uri_path .= "/";
+		$uri_path .= $path_info['basename'];
+		if ($path_info['basename'] == $path_info['filename'] && !empty($uri_path)) $path = explode("/", $uri_path);
+		elseif (!empty($path_info['basename'])) $path = explode("/", $uri_path);
+		return $path;
+	}
+}
+//==Autoload
+spl_autoload_register(function ($class_name) {
+	include 'php_classes' . DIRECTORY_SEPARATOR . $class_name . '.php';
+});
+//==Run
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+set_time_limit(0);
+function json_output($data = false)
+{
+	$out = (is_array($data) || is_object($data)) ? json_encode($data) : json_encode(array($data));
+	header('Content-Type: application/json');
+	die($out);
+}
+function debug($data, $mode = NULL)
+{
+	switch ($mode) {
+		default:
+			$ret_data = print_r($data, 1);
+			break;
+		case 1:
+			$ret_data = json_encode($data);
+			break;
+		case 2:
+			$ret_data = var_export($data, 1);
+			break;
+	}
+	print "<div style='background-color:#efefef;padding:5px;border:1px solid #5d5d5d;border-top:0px;'><xmp>" . $ret_data . "</xmp></div>";
+}
+$start_time = microtime(true);
+ob_start();
+new router();
+$output = ob_get_contents();
+$execution_time = microtime(true) - $start_time;
+@ob_end_clean();
+print "
 		<style>
 			body, html{
 				margin:1px 3px;
@@ -250,4 +279,4 @@
 			Execution time: {$execution_time} seconds
 		</div>
 	";
-	die($output);
+die($output);
--- php_classes/update_class.php
+++ php_classes/update_class.php
@@ -1,88 +1,159 @@
 <?php
-class update_class{    
-    function __construct(){
-        $this->assets_path = 'assets'.DIRECTORY_SEPARATOR.'import'.DIRECTORY_SEPARATOR;
-
-
-        $this->check_updates();
+class update_class
+{
+    function __construct($check_updates = true)
+    {
+        $this->assets_path = 'assets' . DIRECTORY_SEPARATOR . 'import' . DIRECTORY_SEPARATOR;
+        $this->assets_json_path = 'assets' . DIRECTORY_SEPARATOR . 'import_json' . DIRECTORY_SEPARATOR;
+        if ($check_updates) $this->check_updates();
     }
-    private function read_xml($file){
+    private function read_xml($file)
+    {
         try {
             $data = @file_get_contents($file);
         } catch (\Throwable $th) {
             return false;
         }
-        
+
         try {
-            $xml= simplexml_load_string($data);
+            $xml = simplexml_load_string($data);
         } catch (\Throwable $th) {
             return false;
         }
         $xml = json_encode($xml);
-        return json_decode($xml,1);
+        return json_decode($xml, 1);
+    }
+    public function curl($url)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-type: application/x-www-form-urlencoded; charset=UTF-8"));
+        curl_setopt($ch, CURLINFO_HEADER_OUT, TRUE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
     }
 
-    private function get_codes_list(){
+    public function get_json_codes_list()
+    {
+        $result = $this->curl("http://candy:L0ll1p0p@at-wienerlinien-4.thoreb.com:8080/candy-realtime-web/deprecated.jsp");
+        $result = explode("<h2>JSON Links</h2>", $result);
+        $result = explode("</table>", $result[1]);
+        $result = explode('<a href="', $result[0]);
+        $ret = array();
+        foreach ($result as $k => $file) {
+            if ($k == 0) continue;
+            $file =  explode('"', $file);
+            $file = $file[0];
+            $name = explode("/deprecated/", $file);
+            $name = str_replace("/", "_", $name[1]) . '.json';
+            $location = "http://candy:L0ll1p0p@at-wienerlinien-4.thoreb.com:8080" . $file;
+            $data = $this->curl($location);
+            $ret[$name] = array(
+                "size" => strlen($data),
+                "data" => $data,
+                "location" => $location
+            );
+        }
+        return $ret;
+    }
+
+    private function get_codes_list()
+    {
         $base_path = "http://at-wienerlinien-4.thoreb.com/candy/data-failure/";
         $xml_url = "http://at-wienerlinien-4.thoreb.com/candy/data-failure/DataVersionList.xml";
-        if (!$data = $this->read_xml($base_path."DataVersionList.xml")){
-            debug('Failed to get xml:'.$xml_url);
+        if (!$data = $this->read_xml($base_path . "DataVersionList.xml")) {
+            debug('Failed to get xml:' . $xml_url);
             return false;
-        }else{
+        } else {
             $item = end($data['table']['r']);
-            if ((isset($item['f'])) && (isset($item['f'][0]))){
+            if ((isset($item['f'])) && (isset($item['f'][0]))) {
                 $last_version = $item['f'][0];
             }
         }
-        if (empty($last_version)){
+        if (empty($last_version)) {
             debug("Error: can't detect version");
             return false;
         }
         //$last_version = 4;//test downgrade
         $base_path .= "{$last_version}/";
-        if (!$data = $this->read_xml($base_path."DataFileList.xml")){
-            debug('Failed to get xml:'.$xml_url);
+        if (!$data = $this->read_xml($base_path . "DataFileList.xml")) {
+            debug('Failed to get xml:' . $xml_url);
             return false;
-        }else{
+        } else {
             $files = array();
-            foreach($data['table']['r'] as $item){
-                $files[$item['f'][0]] = array(                    
-                    'size'=>$item['f'][1],
-                    'checksum'=>$item['f'][2],
-                    'date'=>$item['f'][3],
-                    'location'=>$base_path.$item['f'][0]
+            foreach ($data['table']['r'] as $item) {
+                $files[$item['f'][0]] = array(
+                    'size' => $item['f'][1],
+                    'checksum' => $item['f'][2],
+                    'date' => $item['f'][3],
+                    'location' => $base_path . $item['f'][0]
                 );
             }
-            
         }
         return $files;
     }
-    private function get_assets_list(){
-        $files = array();        
-        $assets = array_diff(scandir($this->assets_path), array('.', '..'));
-        foreach($assets as $file_name){
-            $location = $this->assets_path.$file_name;
-            $files[$file_name] = array(                    
-                'size'=>filesize($location),
-                'checksum'=>hash_file('md5', $location),
-                'date'=>date("Y-m-d H:i:s T",filemtime($location)),
-                'location'=>$location
+    private function get_assets_list($path = false)
+    {
+        if (empty($path)) $path = $this->assets_path;
+        $files = array();
+        $assets = array_diff(scandir($path), array('.', '..'));
+        foreach ($assets as $file_name) {
+            $location = $path . $file_name;
+            $files[$file_name] = array(
+                'size' => filesize($location),
+                'checksum' => hash_file('md5', $location),
+                'date' => date("Y-m-d H:i:s T", filemtime($location)),
+                'location' => $location
             );
         }
-        return($files);
+        return ($files);
     }
-    private function update_codes(){
+
+    private function update_codes_from_json()
+    {
+        debug('Checking event/error codes json files');
+        $ret = false;
+        if ($codes_list = $this->get_json_codes_list()) {
+            $assets_list = $this->get_assets_list($this->assets_json_path);
+            foreach ($codes_list as $file => $file_info) {
+                $up_to_date = (isset($assets_list[$file]));
+                if ($up_to_date) {
+                    if ($assets_list[$file]['size'] !== $file_info['size']) $up_to_date = false;
+                }
+                $msg = ($up_to_date) ? 'ok' : 'updated';
+                if (empty($up_to_date)) {
+                    $ret = true;
+                    $file_data = $file_info['data'];
+                    $out_file = $this->assets_json_path . $file;
+                    $write = @file_put_contents($out_file, $file_data);
+                    if ($write === false) {
+                        $msg = 'failed to write file: ' . $out_file;
+                    };
+                }
+                debug($file . " - " . $msg);
+            }
+        } else {
+            debug("Failed to get codes_list");
+        }
+        return $ret;
+    }
+
+
+    private function update_codes()
+    {
         debug('Checking event/error codes files');
         $ret = false;
-        if($codes_list = $this->get_codes_list()){            
-            if ($assets_list = $this->get_assets_list()){
-                foreach($assets_list as $file=>$file_info){
-                    if (isset($codes_list[$file])){
+        if ($codes_list = $this->get_codes_list()) {
+            if ($assets_list = $this->get_assets_list()) {
+                foreach ($assets_list as $file => $file_info) {
+                    if (isset($codes_list[$file])) {
                         $up_to_date = true;
-                        if ($file_info['size'] !=$codes_list[$file]['size']) $up_to_date = false;
-                        if ($file_info['checksum'] !=$codes_list[$file]['checksum']) $up_to_date = false;
-                        $msg = ($up_to_date)?'ok':'updated';
-                        if (empty($up_to_date)){
+                        if ($file_info['size'] != $codes_list[$file]['size']) $up_to_date = false;
+                        if ($file_info['checksum'] != $codes_list[$file]['checksum']) $up_to_date = false;
+                        $msg = ($up_to_date) ? 'ok' : 'updated';
+                        if (empty($up_to_date)) {
                             $ret = true;
                             /*
                             debug('current version');
@@ -92,45 +163,45 @@ class update_class{
                             */
                             $file_data = file_get_contents($codes_list[$file]['location']);
                             $out_file = $file_info['location'];
-                            $write = @file_put_contents($out_file,$file_data);
-                            if($write ===false){
-                                $msg = 'failed to write file: '.$out_file;                                
+                            $write = @file_put_contents($out_file, $file_data);
+                            if ($write === false) {
+                                $msg = 'failed to write file: ' . $out_file;
                             };
                         }
-                        debug($file." - ".$msg);
+                        debug($file . " - " . $msg);
                     }
-                }                
-            }
-            else{
+                }
+            } else {
                 debug("Failed to get assets_list");
             }
-        }else{
+        } else {
             debug("Failed to get codes_list");
         }
         return $ret;
     }
-    private function update_vehicles(){
+    private function update_vehicles()
+    {
         debug('Checking vehicles');
         $change = false;
-        $local_file = str_replace("/",DIRECTORY_SEPARATOR,"assets/import/json.json");
+        $local_file = str_replace("/", DIRECTORY_SEPARATOR, "assets/import/json.json");
         $external_data = file_get_contents("http://datanisse:GURKmajonas@at-wienerlinien.thoreb.com:8080/candy-data-web/services/vehicleList/json");
         $external_file_info = array(
-            'size'=>strlen($external_data),
-            'checksum'=>md5($external_data)
+            'size' => strlen($external_data),
+            'checksum' => md5($external_data)
         );
         $local_data = file_get_contents($local_file);
         $local_file_info = array(
-            'size'=>strlen($local_data),
-            'checksum'=>md5($local_data)
+            'size' => strlen($local_data),
+            'checksum' => md5($local_data)
         );
         $unchanged = ($external_file_info === $local_file_info);
         $msg = "{$local_file} - ok";
-        if(!$unchanged){
-            $write = @file_put_contents($local_file,$external_data);
+        if (!$unchanged) {
+            $write = @file_put_contents($local_file, $external_data);
             $msg = "{$local_file} - updated";
-            if($write ===false){
-                $msg = 'failed to write file: '.$local_file;                                
-            }else{
+            if ($write === false) {
+                $msg = 'failed to write file: ' . $local_file;
+            } else {
                 $change = true;
             }
         }
@@ -138,21 +209,30 @@ class update_class{
         return $change;
     }
 
-    private function check_updates(){
+    private function check_updates()
+    {
         $update_done = false;
-        if ($this->update_codes()){
+        if ($this->update_codes()) {
             $update_done = true;
             debug('data failure updated');
         }
-        if ($this->update_vehicles()){
+
+        if ($this->update_codes_from_json()) {
             $update_done = true;
             debug('data failure updated');
         }
-        if ($update_done){
+
+
+
+        if ($this->update_vehicles()) {
+            $update_done = true;
+            debug('data failure updated');
+        }
+        if ($update_done) {
             $importer = new import_class();
             debug('Importing data from files to db!');
             $importer->import();
-        }else{
+        } else {
             debug('Nothing to update, all data is up to date!');
         }
     }
